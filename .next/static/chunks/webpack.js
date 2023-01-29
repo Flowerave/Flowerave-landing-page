7501eabc0a1bbca5b4e703d7038146685b937540
outjam <flowerave@proton.me>
2023-01-29 22:25:33 +0300

    Fix Wrap

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "060a0e7a417505de"; }
+/******/ 		__webpack_require__.h = function() { return "14a7b9abcfa5f0a8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
