6e5fae5b14ea4d7f09149ee715c548b8b5244c03
outjam <flowerave@proton.me>
2023-01-30 02:01:36 +0300

    add

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "823e72fe7c37e163"; }
+/******/ 		__webpack_require__.h = function() { return "491db72f5d843e31"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
