3419a28059a18d444272c7f04fbc7336580367d8
outjam <flowerave@proton.me>
2023-01-29 19:24:36 +0300

    Add

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1838e8631e5f850e"; }
+/******/ 		__webpack_require__.h = function() { return "100e8f300fbcb005"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/componentns/SectionRose/SectionRose.js
+++ src/componentns/SectionRose/SectionRose.js
@@ -5,8 +5,8 @@ function SectionRose() {
     <div id='sectionRose' className='sectionRose'>
         <span className='inWork'>Section Rose in Work</span>
 
-        <h2 className='body'>Flowerave is a project based on TON Blockchain technology created in collaboration between the team of lead by Culture3k and Moscow based record label System (108)</h2>
-        <p className='description'>Flowerave developers have implemented a Telegram bot functioning to sell event tickets in TON cryptocurrency. The bot itself will leverage the power of TON blockchain to facilitate peer-to-peer payments, secure contracts and data storage. Once the user makes a payment, the bot also updates their account in TON blockchain, issuing an NFT ticket. This NFT functions as a pass for the event - when presented to the host, the NFT will be scanned and compared to TON blockchain records to confirm the purchase and continue with the registration process.</p>
+        <h2 className='headerline'>Flowerave is a project based on TON Blockchain technology created in collaboration between the team of lead by Culture3k and Moscow based record label System (108)</h2>
+        <p className='body'>Flowerave developers have implemented a Telegram bot functioning to sell event tickets in TON cryptocurrency. The bot itself will leverage the power of TON blockchain to facilitate peer-to-peer payments, secure contracts and data storage. Once the user makes a payment, the bot also updates their account in TON blockchain, issuing an NFT ticket. This NFT functions as a pass for the event - when presented to the host, the NFT will be scanned and compared to TON blockchain records to confirm the purchase and continue with the registration process.</p>
 
     </div>
   )
