1e459fcbe72e137e80baf567e949ece36dc5b864
outjam <flowerave@proton.me>
2023-01-29 15:46:24 +0300

    Add Style

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "22c823fa92eb2692"; }
+/******/ 		__webpack_require__.h = function() { return "2bbfdf01adec832a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
