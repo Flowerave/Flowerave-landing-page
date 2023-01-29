0b5895d7cadb4ec3d626f710b8211558f10cbcb7
outjam <flowerave@proton.me>
2023-01-29 21:59:41 +0300

    Fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "acb7643f3a87fc9a"; }
+/******/ 		__webpack_require__.h = function() { return "2e6134a2c41c122d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/componentns/SectionRose/SectionRose.js
+++ src/componentns/SectionRose/SectionRose.js
@@ -4,7 +4,7 @@ function SectionRose() {
   return (
     <div id='sectionRose' className='sectionRose'>
         <h2 className='headerline headerline4'>Flowerave is a project based on TON Blockchain technology created in collaboration between the team of lead by Culture3k and Moscow based record label System (108)</h2>
-        <p className='body'>Flowerave developers have implemented a Telegram bot functioning to sell event tickets in TON cryptocurrency. The bot itself will leverage the power of TON blockchain to facilitate peer-to-peer payments, secure contracts and data storage. Once the user makes a payment, the bot also updates their account in TON blockchain, issuing an NFT ticket. This NFT functions as a pass for the event - when presented to the host, the NFT will be scanned and compared to TON blockchain records to confirm the purchase and continue with the registration process.</p>
+        <p className='description'>Flowerave developers have implemented a Telegram bot functioning to sell event tickets in TON cryptocurrency. The bot itself will leverage the power of TON blockchain to facilitate peer-to-peer payments, secure contracts and data storage. Once the user makes a payment, the bot also updates their account in TON blockchain, issuing an NFT ticket. This NFT functions as a pass for the event - when presented to the host, the NFT will be scanned and compared to TON blockchain records to confirm the purchase and continue with the registration process.</p>
         
         <div className='roseMetro'>
             {/* Row 1 */}
