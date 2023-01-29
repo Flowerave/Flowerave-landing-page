d0a9a56aa32936b7020bb92b9cdf96e024fea6d4
outjam <flowerave@proton.me>
2023-01-29 21:31:03 +0300

    Add Rose

--- src/componentns/SectionMain/SectionMain.js
+++ src/componentns/SectionMain/SectionMain.js
@@ -4,9 +4,11 @@ import Button from '../Button/Button';
 function SectionMain() {
   return (
     <div id='sectionMain' className='sectionMain'>
+        {/* Main Video */}
         <video className="video" loop={'true'} muted={'true'} autoplay={'true'} playsinline={'true'}>
             <source src="/video.mp4" type="video/mp4" />
         </video>
+        {/* Content */}
         <div className='content'>
             <h1 className='headerline headerline2'>Unlock Your Possibilities with Flowerave: TON Blockchain Technology and NFTs!</h1>
             <div className='row'>
--- src/componentns/SectionRose/SectionRose.js
+++ src/componentns/SectionRose/SectionRose.js
@@ -4,8 +4,75 @@ function SectionRose() {
   return (
     <div id='sectionRose' className='sectionRose'>
         <h2 className='headerline headerline4'>Flowerave is a project based on TON Blockchain technology created in collaboration between the team of lead by Culture3k and Moscow based record label System (108)</h2>
-        <p className='description'>Flowerave developers have implemented a Telegram bot functioning to sell event tickets in TON cryptocurrency. The bot itself will leverage the power of TON blockchain to facilitate peer-to-peer payments, secure contracts and data storage. Once the user makes a payment, the bot also updates their account in TON blockchain, issuing an NFT ticket. This NFT functions as a pass for the event - when presented to the host, the NFT will be scanned and compared to TON blockchain records to confirm the purchase and continue with the registration process.</p>
-
+        <p className='body'>Flowerave developers have implemented a Telegram bot functioning to sell event tickets in TON cryptocurrency. The bot itself will leverage the power of TON blockchain to facilitate peer-to-peer payments, secure contracts and data storage. Once the user makes a payment, the bot also updates their account in TON blockchain, issuing an NFT ticket. This NFT functions as a pass for the event - when presented to the host, the NFT will be scanned and compared to TON blockchain records to confirm the purchase and continue with the registration process.</p>
+        
+        <div className='roseMetro'>
+            {/* Row 1 */}
+            <div className='row'>
+                {/* Rose 1 */}
+                <video className="rose" loop={'true'} muted={'true'} autoplay={'true'} playsinline={'true'}>
+                    <source src="/rose1.mp4" type="video/mp4" />
+                </video>
+                {/* Rose 2 */}
+                <video className="rose" loop={'true'} muted={'true'} autoplay={'true'} playsinline={'true'}>
+                    <source src="/rose2.mp4" type="video/mp4" />
+                </video>
+                {/* Rose 3 */}
+                <video className="rose" loop={'true'} muted={'true'} autoplay={'true'} playsinline={'true'}>
+                    <source src="/rose3.mp4" type="video/mp4" />
+                </video>
+                {/* Rose 4 */}
+                <video className="rose" loop={'true'} muted={'true'} autoplay={'true'} playsinline={'true'}>
+                    <source src="/rose4.mp4" type="video/mp4" />
+                </video>
+                {/* Rose 5 */}
+                <video className="rose" loop={'true'} muted={'true'} autoplay={'true'} playsinline={'true'}>
+                    <source src="/rose1.mp4" type="video/mp4" />
+                </video>
+            </div>
+            {/* Row 2 */}
+            <div className='row'>
+                {/* Rose 6 */}
+                <video className="rose" loop={'true'} muted={'true'} autoplay={'true'} playsinline={'true'}>
+                    <source src="/rose1.mp4" type="video/mp4" />
+                </video>
+                {/* Rose 7 */}
+                <video className="rose" loop={'true'} muted={'true'} autoplay={'true'} playsinline={'true'}>
+                    <source src="/rose1.mp4" type="video/mp4" />
+                </video>
+                {/* Rose 8 */}
+                <video className="rose" loop={'true'} muted={'true'} autoplay={'true'} playsinline={'true'}>
+                    <source src="/rose1.mp4" type="video/mp4" />
+                </video>
+                {/* Rose 9 */}
+                <video className="rose" loop={'true'} muted={'true'} autoplay={'true'} playsinline={'true'}>
+                    <source src="/rose1.mp4" type="video/mp4" />
+                </video>
+            </div>
+            {/* Row 3 */}
+            <div className='row'>
+                {/* Rose 10 */}
+                <video className="rose" loop={'true'} muted={'true'} autoplay={'true'} playsinline={'true'}>
+                    <source src="/rose1.mp4" type="video/mp4" />
+                </video>
+                {/* Rose 11 */}
+                <video className="rose" loop={'true'} muted={'true'} autoplay={'true'} playsinline={'true'}>
+                    <source src="/rose1.mp4" type="video/mp4" />
+                </video>
+                {/* Rose 12 */}
+                <video className="rose" loop={'true'} muted={'true'} autoplay={'true'} playsinline={'true'}>
+                    <source src="/rose1.mp4" type="video/mp4" />
+                </video>
+                {/* Rose 13 */}
+                <video className="rose" loop={'true'} muted={'true'} autoplay={'true'} playsinline={'true'}>
+                    <source src="/rose1.mp4" type="video/mp4" />
+                </video>
+                {/* Rose 14 */}
+                <video className="rose" loop={'true'} muted={'true'} autoplay={'true'} playsinline={'true'}>
+                    <source src="/rose1.mp4" type="video/mp4" />
+                </video>
+            </div>
+        </div>
     </div>
   )
 }
