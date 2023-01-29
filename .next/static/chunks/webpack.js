2a9840a07a21caa2814d4616fe41fd63f3957219
outjam <flowerave@proton.me>
2023-01-29 15:55:01 +0300

    Animation

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "470e8c3aad014e3f"; }
+/******/ 		__webpack_require__.h = function() { return "aebf824e97afb33f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
