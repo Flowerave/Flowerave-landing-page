298d822261a6917db77135c220bf94e3717cf8b6
outjam <flowerave@proton.me>
2023-01-29 14:20:11 +0300

    Fix Main Video

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f12722801e4ff217"; }
+/******/ 		__webpack_require__.h = function() { return "32fdd489e5c2ed19"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/componentns/SectionMain/SectionMain.js
+++ src/componentns/SectionMain/SectionMain.js
@@ -4,8 +4,8 @@ import Button from '../Button/Button';
 function SectionMain() {
   return (
     <div id='sectionMain' className='sectionMain'>
-        <video className='video' autoPlay={true} loop={true} muted={true} autoplay>
-            <source src="/video.mp4" />
+        <video autoplay="true" className="video" loop="true">
+            <source src="/video.mp4" type="video/mp4" />
         </video>
         <div className='content'>
             <h1 className='headerline headerline1'>Unlock Your Possibilities with Flowerave: TON Blockchain Technology and NFTs!</h1>
