97399536c55c6c3e9c42f0edc444bda291a0adab
outjam <flowerave@proton.me>
2023-01-30 12:20:13 +0300

    Add icons

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c98cf2e1b9c59f59"; }
+/******/ 		__webpack_require__.h = function() { return "557991f30855c721"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
