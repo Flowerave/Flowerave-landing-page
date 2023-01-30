d03c559aabc4ed8fcacb214ce1adbf43fe28b9ae
outjam <flowerave@proton.me>
2023-01-31 00:34:06 +0300

    Add Style BTN

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6c7fc44c58731fe6"; }
+/******/ 		__webpack_require__.h = function() { return "71b79bf6bf4b17b8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
