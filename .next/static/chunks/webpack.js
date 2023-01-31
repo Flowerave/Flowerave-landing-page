d1ab4c17bc17961e7dcce715afc2305b8725817f
outjam <flowerave@proton.me>
2023-01-31 18:55:19 +0300

    add

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0130bdb97c81aaa5"; }
+/******/ 		__webpack_require__.h = function() { return "a0f94f4192edc302"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/componentns/SectionRoadmap/SectionRoadmapMobile/SectionRoadmapMobile.js
+++ src/componentns/SectionRoadmap/SectionRoadmapMobile/SectionRoadmapMobile.js
@@ -6,13 +6,23 @@ function SectionRoadmapMobile() {
         <h5>Roadmap</h5>
         <div className='mobileRoadmapList'>
             <div className='mobileRoadmapList_Card'>
-                <p className='description'>Body</p>
-                <p className='caption'>Description</p>
+                <p className='description'>Ticket</p>
+                <p className='caption'>NFT Events</p>
                 <span className='inWork'>Coming Soon</span>
             </div>
             <div className='mobileRoadmapList_Card'>
-                <p className='description'>Body</p>
-                <p className='caption'>Description</p>
+                <p className='description'>VPN</p>
+                <p className='caption'>Pay Ton</p>
+                <span className='inWork'>Coming Soon</span>
+            </div>
+            <div className='mobileRoadmapList_Card'>
+                <p className='description'>Screen</p>
+                <p className='caption'>Wallet</p>
+                <span className='inWork'>Coming Soon</span>
+            </div>
+            <div className='mobileRoadmapList_Card'>
+                <p className='description'>Battle</p>
+                <p className='caption'>NFT game</p>
                 <span className='inWork'>Coming Soon</span>
             </div>
         </div>
