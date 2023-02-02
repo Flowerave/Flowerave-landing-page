1b92ef05ea69cca6c3a20d2a38adb71ed4399ea2
outjam <flowerave@proton.me>
2023-02-02 13:07:40 +0300

    fix

--- src/componentns/SectionService/Card/Card.js
+++ src/componentns/SectionService/Card/Card.js
@@ -12,7 +12,7 @@ function Card(props) {
     useEffect(() => {
         gsap.to(".v-a", {
             duration: 10,
-            y: "-32px",
+            y: "-16px",
             transition: "rotate(30deg)",
             scrollTrigger: {
                 trigger: ".card_Service",
